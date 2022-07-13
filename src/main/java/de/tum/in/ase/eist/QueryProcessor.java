f5f5bb30d1788497054ffd2c6c5c9bcf87c56a7f
Muhammed Emre Bayraktaroglu <muhammed.emre.bayraktaroglu@tum.de>
2022-07-13 15:27:01 +0200

    f u

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -12,11 +12,14 @@ public class QueryProcessor {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
-        } else if (query.contains("name")) {
+
+        } 
+        else if (query.contains("what is your name")) {
+            return "Emre ur best friend:)";}
+        else if (query.contains("name")) {
            return "MyTeam";
-        } else if (query.contains(" what is your name")) {
-            return "Emre ur best friend:)";
-        } else { // TODO extend the programm here
+        } 
+        else { // TODO extend the programm here
             return "";
         }
     }
