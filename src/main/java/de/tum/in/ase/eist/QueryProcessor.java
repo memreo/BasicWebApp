9d2bd89745f8743d23911cd9d71bba8f58753999
Muhammed Emre Bayraktaroglu <muhammed.emre.bayraktaroglu@tum.de>
2022-07-13 15:30:18 +0200

    a

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -14,7 +14,7 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
 
         } 
-        else if (query.contains("what is your name")) {
+        else if (query.contains(" what is your name")) {
             return "Emre ur best friend:)";}
         else if (query.contains("name")) {
            return "MyTeam";
